5fabbb0eaf34f25665ef8403593787f52da340c0
Stephen Fluin <stephen.fluin@gmail.com>
2016-08-26 10:13:02 -0700

    Materialize profile

--- src/app/developers/expert-form.component.ts
+++ src/app/developers/expert-form.component.ts
@@ -11,15 +11,15 @@ import { AngularFire, FirebaseListObservable } from 'angularfire2';
     selector: 'expert-form',
     template: `
     <form *ngIf="expert" (submit)="save(expert)" ngNoForm>
-        <label>Name <input name="name" [(ngModel)]="expert.name"></label>
-        <label>Location <input name="location" [(ngModel)]="expert.location"></label>
-        <label>Bio <input name="bio" [(ngModel)]="expert.bio"></label>
-        <label>Pic URL <img *ngIf="expert.picUrl" [src]="expert.picUrl" style="max-height:1em;"> <input name="picUrl" [(ngModel)]="expert.picUrl"></label>
+        <div><md-input placeholder="Name" [(ngModel)]="expert.name"></md-input></div>
+        <div><md-input placeholder="Location" [(ngModel)]="expert.location"></md-input></div>
+        <div><md-input placeholder="Bio" [(ngModel)]="expert.bio"></md-input></div>
+        <div><md-input placeholder="Pic URL" [(ngModel)]="expert.picUrl"></md-input> <img *ngIf="expert.picUrl" [src]="expert.picUrl" style="max-height:1em;"></div>
 
-        <label>Website <input name="website" [(ngModel)]="expert.website"></label>
-        <label>Twitter <input name="twitter" [(ngModel)]="expert.twitter"></label>
-        <label>Github Username <input name="github" [(ngModel)]="expert.github"></label>
-        <label>LinkedIn Username <input name="linkedIn" [(ngModel)]="expert.linkedIn"></label>
+        <div><md-input placeholder="Website" [(ngModel)]="expert.website"></md-input></div>
+        <div><md-input placeholder="Twitter" [(ngModel)]="expert.twitter"></md-input></div>
+        <div><md-input placeholder="Github Username" [(ngModel)]="expert.github"></md-input></div>
+        <div><md-input placeholder="LinkedIn Username" [(ngModel)]="expert.linkedIn"></md-input></div>
         <!--<div>Communities</div>
         <picker [list]="'/communities/'" [order]="'name'" [selectedKeys]="expert.communities" (update)="chooseCommunities($event)"></picker>
         -->
@@ -40,8 +40,8 @@ import { AngularFire, FirebaseListObservable } from 'angularfire2';
                 </label>
 
                         
-                <label>Title <input placeholder="Content Title" [(ngModel)]="newContent.title"></label>
-                <label>URL <input placeholder="URL"[(ngModel)]="newContent.url"></label>
+                <div><md-input placeholder="Title" placeholder="Content Title" [(ngModel)]="newContent.title"></md-input></div>
+                <div><md-input placeholder="URL" placeholder="URL" [(ngModel)]="newContent.url"></md-input></div>
                 <div class="options">
                     <button md-raised-button color="primary" type="button" (click)="createContent();">Create</button>
                 </div>
