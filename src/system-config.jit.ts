7b7d8a6b337dde68a4a0f272048b3660b3a4bcfa
Stephen Fluin <stephen.fluin@gmail.com>
2016-09-12 12:32:30 -0700

    RC6 Update

--- src/system-config.jit.ts
+++ src/system-config.jit.ts
@@ -1,21 +1,16 @@
-/***********************************************************************************************
- * User Configuration.
- **********************************************************************************************/
-/** Map relative paths to URLs. */
-const map: any = {
-  '@angular2-material': 'vendor/@angular2-material',
-  
-  'firebase': 'vendor/firebase/firebase.js',
-  'angularfire2': 'vendor/angularfire2',
-};
-
-/** User packages configuration. */
-const packages: any = {
+let packages = {
+  app: {
+    main: './main.js',
+    defaultExtension: 'js'
+  },
+  rxjs: {
+    defaultExtension: 'js'
+  },
   angularfire2: {
     defaultExtension: 'js',
     main: 'angularfire2.js'
   }
-};
+}
 
 // put the names of any of your Material components here
 const materialPkgs:string[] = [
@@ -26,54 +21,45 @@ const materialPkgs:string[] = [
   'card',
   'slide-toggle',
 ];
-
 materialPkgs.forEach((pkg) => {
-  packages[`@angular2-material/${pkg}`] = {main: `${pkg}.js`};
+  packages[`@angular2-material/${pkg}`] = {
+    main: `${pkg}.umd.js`
+  };
 });
 
-////////////////////////////////////////////////////////////////////////////////////////////////
-/***********************************************************************************************
- * Everything underneath this line is managed by the CLI.
- **********************************************************************************************/
-const barrels: string[] = [
-  // Angular specific barrels.
-  '@angular/core',
-  '@angular/common',
-  '@angular/compiler',
-  '@angular/router',
-  '@angular/forms',
-  '@angular/http',
-  '@angular/platform-browser',
-  '@angular/platform-browser-dynamic',
 
-  // Thirdparty barrels.
-  'rxjs',
-  'firebase',
-
-  // App specific barrels.
-  'app',
-  'app/shared',
-  'ngfactory',
-  /** @cli-barrel */
-];
-
-const cliSystemConfigPackages: any = {};
-barrels.forEach((barrelName: string) => {
-  cliSystemConfigPackages[barrelName] = { main: 'index' };
-});
-
-/** Type declaration for ambient System. */
-declare var System: any;
-
-// Apply the CLI SystemJS configuration.
 System.config({
-  map: {
-    '@angular': 'vendor/@angular',
-    'rxjs': 'vendor/rxjs',
-    'main': 'main.js'
+  paths: {
+    'npm:': 'vendor/'
   },
-  packages: cliSystemConfigPackages
+  map: {
+    'main': './main.js',
+
+    '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
+    '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
+    '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
+    '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
+    '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
+    '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
+    '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
+    '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
+
+
+    '@angular/core/testing': 'npm:@angular/core/bundles/core-testing.umd.js',
+    '@angular/common/testing': 'npm:@angular/common/bundles/common-testing.umd.js',
+    '@angular/compiler/testing': 'npm:@angular/compiler/bundles/compiler-testing.umd.js',
+    '@angular/http/testing': 'npm:@angular/http/bundles/http-testing.umd.js',
+    '@angular/platform-browser/testing': 'npm:@angular/platform-browser/bundles/platform-browser-testing.umd.js',
+    '@angular/platform-browser-dynamic/testing': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic-testing.umd.js',
+    '@angular/router/testing': 'npm:@angular/router/bundles/router-testing.umd.js',
+
+    'rxjs': 'npm:rxjs',
+
+    '@angular2-material': 'vendor/@angular2-material',
+    'firebase': 'vendor/firebase/firebase.js',
+    'angularfire2': 'vendor/angularfire2',
+  },
+  //packages defines our app package
+  packages: packages
 });
 
-// Apply the user's configuration.
-System.config({ map, packages });
