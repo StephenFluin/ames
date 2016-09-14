ecce3e74c66f0a3073b4c1a0a360f2d687289ca8
Stephen Fluin <stephen.fluin@gmail.com>
2016-09-14 08:57:43 -0700

    AOT cleanup

--- src/main-static.ts
+++
@@ -1,8 +0,0 @@
-import { platformBrowser } from '@angular/platform-browser';
-import { MyAppModuleNgFactory } from './ngfactory/app/app.module.ngfactory';
-
-platformBrowser().bootstrapModuleFactory(MyAppModuleNgFactory);
-
-// platform browser dynamic - runtime compilation (dynamic compilation in the browser)
-// platform browser - static compilation (also used by platform-browser-dynamic) in browser
-// platform server - run angular without DOM
